faa7598ec1770a2e3fc210ba53339952b1ca51be
João Felipe <jffssd@gmail.com>
2017-12-13 11:22:09 -0200

    new

--- dev/sql/06 - Equipes.sql
+++ dev/sql/06 - Equipes.sql
@@ -10,4 +10,10 @@ INSERT INTO EQUIPE (ID, NOME, SIGLA, REGIAO_ID, STATUS, PAIS_ID, SEDE_ID, TECNIC
 (8, 'Team One', 'TONE', 1, 'Ativo', 33, 1, 1, 15 , 'team-one-logo.png', '#828282', '#f4f142', '#000000'),
 (9, 'Iron Hawks', 'IHW', 1, 'Ativo', 33, 1, 1, 15 , 'iron-hawks-logo.png', '#b20101', '#000000', '#000000'),
 (10, 'Merciless Gaming', 'MLG', 1, 'Ativo', 33, 1, 1, 15 , 'merciless-gaming-logo.png', '#0083db','#ffffff', '#000000'),
-(11, 'Ilha da Macacada', 'IDM', 1, 'Ativo', 33, 1, 1, 15, 'ilha-da-macacada-logo.png', '#f4f142', '#000000', '#000000');
+(11, 'Ilha da Macacada', 'IDM', 1, 'Ativo', 33, 1, 1, 15, 'ilha-da-macacada-logo.png', '#f4f142', '#000000', '#000000'),
+(12, 'CNB Blue', 'CNB-B', 1, 'Ativo', 33, 1, 1, 15 , 'cnb-esports-logo.png', '#0083db', '#ffffff', '#000000'),
+(13, 'CNB Trinity White', 'CNB-TW', 1, 'Ativo', 33, 1, 1, 15 , 'cnb-esports-logo.png', '#0083db', '#ffffff', '#000000'),
+(14, 'Flamento E-sports', 'FLA', 1, 'Ativo', 33, 1, 1, 15, 'pain-gaming-logo.png', '#b20101', '#000000', '#000000'),
+(15, 'INTZ E-sports', 'ITZ', 1, 'Ativo', 33, 1, 1, 15, 'intz-logo.png', '#ffffff', '#000000', '#000000'),
+(16, 'Kabum E-sports', 'VFK', 1, 'Ativo', 33, 1, 1, 15 , 'kabum-esports-logo.png', '#828282', '#f4f142', '#000000'),
+(17, 'T-SHOW', 'TSW', 1, 'Ativo', 33, 1, 1, 15 , 't-show-esports-logo.png', '#828282', '#f4f142', '#000000');
