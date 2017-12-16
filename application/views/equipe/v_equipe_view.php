d5e2676bcf80e697e37ef699e65480238038dcab
João Felipe <jffssd@gmail.com>
2017-12-16 11:41:51 -0200

    new

--- application/views/equipe/v_equipe_view.php
+++ application/views/equipe/v_equipe_view.php
@@ -15,47 +15,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				?>
 			</ul>
 		</div>
-		<div>
 
-		
-		</div>
-
-		<div class="col-md-12" style="background-color: #f5f5f5; border: 1px solid #e2e2e2; border-radius: 7px;">
+		<div class="col-md-12">
 			<div class="row">
 				<div class="col-md-4">
-					<div class="logo-equipe">
-						<div class="row"  style="background-color: white; margin-top: 10px; border: 1px solid #2f3f3f3; border-radius: 7px;">
-								<?php echo '<img src="'.site_url().'/assets/img/logo-equipes/'.$logo.'" width="280" height="280">'; ?>
-						</div>
-						<div class="row">
-							<?php echo '<h3 align="center">'.$nome.'</h1>'; ?>
-						</div>
-						<div class="row">
-							<?php 
-								foreach($paises -> result() as $p_s){
-									if ($p_s->id == $pais){
-										echo '<h5 align="center">'.$sigla.'</h3>';
-										echo '<img src="'.site_url().'assets/img/bandeiras/'.$p_s->name.'.png" width="30" height="20" alt="'.$p_s->nome.'">';
+					<div class="logo-equipe" style="background-color: black;">
+						<div class="row" align="center"  style="height: 80%; background-color: blue;margin-top: 10px; border: 1px solid #2f3f3f3; border-radius: 7px;   display: flex; align-items: center;  justify-content: center;">
+							<?php 	echo '<img src="'.site_url().'/assets/img/logo-equipes/'.$logo.'" width="320" height="320">'; 
+									echo '<h3 align="center">'.$nome.'</h1>';
+									foreach($paises -> result() as $p_s){
+										if ($p_s->id == $pais){
+											echo '<h5 align="center">'.$sigla.'</h3>';
+											echo '<img src="'.site_url().'assets/img/bandeiras/'.$p_s->name.'.png" width="30" height="20" alt="'.$p_s->nome.'">';
+										}
 									}
-								}
+
+									foreach($regioes -> result() as $r_s){
+										if ($r_s->id == $regiao){
+											echo '<img src="'.site_url().'assets/img/logo-ligas/'.$r_s->sigla.'.png" width="30" height="20" alt="'.$r_s->sigla.'">';
+											
+										}
+									}
 							?>
-						</div>
-						<div class="row">
-                    	<?php
-						foreach($regioes -> result() as $r_s){
-								if ($r_s->id == $regiao){
-									echo '<img src="'.site_url().'assets/img/logo-ligas/'.$r_s->sigla.'.png" width="30" height="20" alt="'.$r_s->sigla.'">';
-									
-								}
-						}
-						?>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="logo-equipe" style="">
+				<div class="col-md-3">
+					<div class="logo-equipe" style="background-color: green;">
 						<div class="row" style="height:200px;">
-
+						
 						</div>
 						<div class="row">
 							<?php
@@ -78,19 +66,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</div>
 				</div>
 
-				<div class="col-md-4" style="height: 488px; ">
+				<div class="col-md-5" style="height: 488px; ">
 				<h2>JG</h2>
 
 				<style>
-
 					.player-profile-rows{
 						margin: 5px;
 					}
 
 					.player-profile-cell {
+						border: 1px solid;
+						border-color: #e4e4e4;
 						height: 140px; 
 						width: 100%; 
-						border: 1px solid black; 
 						border-radius: 10px; 
 						padding: 10px;
 					}
@@ -102,6 +90,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					}
 
 					.player-profile-info {
+						padding-left: 5px;
 						height:120px; 
 						width: 60%;
 					}
@@ -113,10 +102,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						display: flex;
   						align-items:center;
   						justify-content:center;
+						margin-right: -20px;
 					}
 
 					.profile-score {
-						font-size: 80px;
+						font-size: 70px;
+
+					}
+
+					.profile-nick{
+						font-size: 20px;
+						font-weight: bold;
+
+					}
+
+					.profile-nick-small{
+						font-size: 16px;
+						font-weight: bold;
 
 					}
 
@@ -124,6 +126,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						margin: 0;
 
 					}
+
+
 
 				</style>
 
@@ -137,27 +141,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										echo '</div>';
 										echo '<div class="player-profile-info">';
 											echo '<div class="player-profile-score">';
-											echo '<h1 class="profile-score">91</h1>';
+											$score_result = $j_e->at_trab + $j_e->at_ment + $j_e->at_consist + $j_e->at_mec + $j_e->at_vis;
+											echo '<h1 class="profile-score">'.$score_result.'</h1>';
 											echo '</div>';
-												echo '<p><strong>'.strtoupper('Souldevourer').'</strong></p>';
-											echo '<p>Brasil</p>';
-											echo '<p><span class="badge badge-info">Atirador</span> <span class="badge badge-info">ADC</span></p>';
-											echo '<p><span class="badge badge-warning">Titular</span></p> ';
+											echo '<div>';
+											$length = strlen($j_e->nick);
+											if($length < 10){
+												foreach($paises -> result() as $p_s){
+													if ($p_s->id == $j_e->pais_id){
+														echo '<p class="profile-nick">'.strtoupper($j_e->nick).' <span><img src="'.site_url().'assets/img/bandeiras/'.$p_s->name.'.png" width="30" height="20" alt="'.$p_s->nome.'"></span></p>';
+													}
+												}
+											}else{
+												foreach($paises -> result() as $p_s){
+													if ($p_s->id == $j_e->pais_id){
+														echo '<p class="profile-nick-small">'.strtoupper($j_e->nick).' <span><img src="'.site_url().'assets/img/bandeiras/'.$p_s->name.'.png" width="30" height="20" alt="'.$p_s->nome.'"></span></p>';
+													}
+												}
+											}
+
+
+											echo '</div>';
+											
+											if($j_e->funcao == 'TOP'){
+												echo '<p><span class="badge badge-danger">Topo</span></p>';
+											}elseif($j_e->funcao == 'MID'){
+												echo '<p><span class="badge badge-info">Meio</span></p>';
+											}elseif($j_e->funcao =="JNG"){
+												echo '<p><span class="badge badge-success">Caçador</span></p>';
+											}elseif($j_e->funcao == "SUP"){
+												echo '<p><span class="badge badge-dark">Suporte</span></p>';
+											}elseif($j_e->funcao == "ADC"){
+												echo '<p><span class="badge badge-primary">Atirador</span></p>';
+											}else{
+
+											}
+
+											if($j_e->titular == 0){
+												echo '<p><span class="badge badge-warning">Titular</span></p> ';
+											}elseif($j_e->titular == 1){
+												echo '<p><span class="badge badge-secondary">Reserva</span></p> ';
+											}
 										echo '</div>';
 									echo '</div>';
 								echo '</div>';
 							echo '</div>';
 							}
 							?>
-
-
-
-					
-
-
-
-
-
 
 				</div>
 			</div>
