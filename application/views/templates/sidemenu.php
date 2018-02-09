9dbf3f57789abb3602ddb5830a1e9c957e4a61e4
João Felipe <jffssd@gmail.com>
2018-02-09 10:42:51 -0200

    new

--- application/views/templates/sidemenu.php
+++ application/views/templates/sidemenu.php
@@ -1,19 +1,27 @@
 <div class="left main-sidebar">
-	
-    <div class="sidebar-inner leftscroll">
-
+	<div class="sidebar-inner leftscroll">
         <div id="sidebar-menu">
-    
-        <ul>
-
+            <ul>
                 <li class="submenu">
-                    <a class="active" href="index.html"><i class="fa fa-fw fa-bars"></i><span> Dashboard </span> </a>
+                    <a class="active" href="index.html"><i class="fa fa-fw fa-bars"></i><span> Painel Principal </span> </a>
                 </li>
 
                 <li class="submenu">
-                    <a href="charts.html"><i class="fa fa-fw fa-area-chart"></i><span> Charts </span> </a>
+                    <a href="#"><i class="fa fa-fw fa-area-chart"></i><span> Perfil </span> </a>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fa fa-fw fa-area-chart"></i><span> Mensagens </span> <span class="label radius-circle bg-success float-right">2</span></a>
                 </li>
                 
+                <li class="submenu">
+                    <a class="carreira-link" href="#"><i class="fa fa-fw fa-table"></i> <span> Carreira </span> <span class="menu-arrow"></span></a>
+                        <ul class="list-unstyled">
+                            <li><a href="tables-basic.html">Jogador <span class="label radius-circle bg-warning float-right">Novo!</span></a></li>
+                            <li><a href="tables-datatable.html">Manager</a></li>
+                        </ul>
+                </li>
+
                 <li class="submenu">
                     <a href="#"><i class="fa fa-fw fa-table"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
                         <ul class="list-unstyled">
@@ -63,29 +71,6 @@
                 </li>
 
                 <li class="submenu">
-                    <a href="#"><i class="fa fa-fw fa-image"></i> <span> Images and Galleries </span> <span class="menu-arrow"></span></a>
-                        <ul class="list-unstyled">
-                            <li><a href="media-fancybox.html"><span class="label radius-circle bg-danger float-right">cool</span> Fancybox </a></li>								
-                            <li><a href="media-masonry.html">Masonry</a></li>
-                            <li><a href="media-lightbox.html">Lightbox</a></li>
-                            <li><a href="media-owl-carousel.html">Owl Carousel</a></li>
-                            <li><a href="media-image-magnifier.html">Image Magnifier</a></li>
-                            
-                        </ul>
-                </li>
-                
-                <li class="submenu">
-                    <a href="#"><span class="label radius-circle bg-danger float-right">20</span><i class="fa fa-fw fa-copy"></i><span> Example Pages </span></a>
-                        <ul class="list-unstyled">								
-                            <li><a href="page-pricing-tables.html">Pricing Tables</a></li>
-                            <li><a target="_blank" href="page-coming-soon.html">Countdown</a></li>								
-                            <li><a href="page-invoice.html">Invoice</a></li>                        
-                            <li><a href="page-login.html">Login / Register</a></li>								
-                            <li><a href="page-blank.html">Blank Page</a></li>
-                        </ul>
-                </li>
-
-                <li class="submenu">
                     <a href="#"><span class="label radius-circle bg-primary float-right">9</span><i class="fa fa-fw fa-indent"></i><span> Menu Levels </span></a>
                         <ul>
                             <li>
@@ -98,21 +83,6 @@
                                         <li><a href="#"><span>Third Level Item</span></a></li>
                                     </ul>
                             </li>                                
-                        </ul>
-                </li>
-
-                <li class="submenu">
-                    <a class="pro" href="#"><i class="fa fa-fw fa-star"></i><span> Pike Admin PRO </span> <span class="menu-arrow"></span></a>
-                        <ul class="list-unstyled">								
-                            <li><a target="_blank" href="https://www.pikeadmin.com/pike-admin-pro">Admin PRO features</a></li>
-                            <li><a href="pro-settings.html">Settings</a></li>
-                            <li><a href="pro-profile.html">My Profile</a></li>
-                            <li><a href="pro-users.html">Users</a></li>
-                            <li><a href="pro-articles.html">Articles</a></li>
-                            <li><a href="pro-categories.html">Categories</a></li>
-                            <li><a href="pro-pages.html">Pages</a></li>								
-                            <li><a href="pro-contact-messages.html">Contact Messages</a></li>
-                            <li><a href="pro-slider.html">Slider</a></li>
                         </ul>
                 </li>
                 
